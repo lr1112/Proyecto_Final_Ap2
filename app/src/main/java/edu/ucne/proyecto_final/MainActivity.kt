cc12d6c19b9b43897b116479c439626da308fc2b
lr1112 <luis_rosario_0324@hotmail.com>
2022-07-22 08:48:17 -0400

    creando proyecto y agregando dependencias

---
+++ app/src/main/java/edu/ucne/proyecto_final/MainActivity.kt
@@ -0,0 +1,43 @@
+package edu.ucne.proyecto_final
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.proyecto_final.ui.theme.Proyecto_FinalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Proyecto_FinalTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Proyecto_FinalTheme {
+        Greeting("Android")
+    }
+}
