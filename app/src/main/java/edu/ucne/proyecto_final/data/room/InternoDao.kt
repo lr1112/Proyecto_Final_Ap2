4e0d737bc8fc95edd1f23faf686e5b3eda123640
lr1112 <luis_rosario_0324@hotmail.com>
2022-07-22 11:10:12 -0400

    creando el internoDao

---
+++ app/src/main/java/edu/ucne/proyecto_final/data/room/InternoDao.kt
@@ -0,0 +1,22 @@
+package edu.ucne.proyecto_final.data.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import edu.ucne.proyecto_final.data.Interno
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface InternoDao {
+    @Query("SELECT * FROM interno")
+    fun selectAll(): Flow<List<Interno>>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(interno: Interno)
+    @Query("DELETE FROM interno WHERE internoId = InternoId")
+    suspend fun delete(internoId: Int)
+    @Query("DELETE FROM Interno ")
+    suspend fun deleteAllInterno()
+
+}
