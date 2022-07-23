de0295530e28f7e71721fc70ee36af4a9ae2168e
lr1112 <luis_rosario_0324@hotmail.com>
2022-07-23 11:16:27 -0400

    agregando la etiqueta AndroidEntryPoint

--- app/src/main/java/edu/ucne/proyecto_final/MainActivity.kt
+++ app/src/main/java/edu/ucne/proyecto_final/MainActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.proyecto_final.ui.theme.Proyecto_FinalTheme
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
