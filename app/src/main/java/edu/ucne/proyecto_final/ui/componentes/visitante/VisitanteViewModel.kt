b4eff4b04000991e8bb1d2f0a106a34f2b9c8a5d
lr1112 <luis_rosario_0324@hotmail.com>
2022-07-25 12:54:26 -0400

    cambiando el nombre al view model

--- app/src/main/java/edu/ucne/proyecto_final/ui/componentes/visitante/VisitanteViewModel.kt
+++ app/src/main/java/edu/ucne/proyecto_final/ui/componentes/visitante/VisitanteViewModel.kt
@@ -12,7 +12,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class OcupacionViewModel @Inject constructor(
+class VisitanteViewModel @Inject constructor(
     val visitanteRepository : VisitanteRepository
 ) : ViewModel(){
     var nombre by mutableStateOf("")
